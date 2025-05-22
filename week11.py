d36705d761fdad755113408964025d47298f80e2
unknown <ygy0518@gmail.com>
2025-05-22 16:05:35 +0900

    v4.3 graph dfs

--- week11.py
+++ week11.py
@@ -1,43 +1,20 @@
-class Graph:
-    def __init__(self, size):
-        self.size = size
-        self.graph = [[0 for _ in range(size)] for _ in range(size)]
+graph = [
+    [0, 1, 1, 0, 0, 0, 0, 0], # A
+    [1, 0, 0, 1, 0, 0, 0, 0], # B
+    [1, 0, 0, 1, 0, 0, 0, 0], # C
+    [0, 1, 1, 0, 1, 1, 1, 0], # D
+    [0, 0, 0, 1, 0, 1, 0, 0], # E
+    [0, 0, 0, 1, 1, 0, 0, 0], # F
+    [0, 0, 0, 1, 0, 0, 0, 1], # G
+    [0, 0, 0, 0, 0, 0, 1, 0]  # H
+]
 
+def dfs(g, i, visited):
+    visited[i] = 1
+    print(chr(ord('A') + i), end=' ')
+    for j in range(len(graph)):
+        if g[i][j] == 1 and not visited[j]:
+            dfs(g, j, visited)
 
-G1 = Graph(4)
-G2 = Graph(4)
-G_self = Graph(4)
-
-# 0 == A, 1 == B, 2 == C, 3 == D
-G1.graph[0][1] = 1; G1.graph[0][2] = 1; G1.graph[0][3] = 1
-G1.graph[1][0] = 1; G1.graph[1][2] = 1
-G1.graph[2][0] = 1; G1.graph[2][1] = 1; G1.graph[2][3] = 1
-G1.graph[3][0] = 1; G1.graph[3][2] = 1
-
-print("G1 무방향 그래프")
-for r in range(G1.size):
-    for c in range(G1.size):
-        print(G1.graph[r][c], end=' ')
-    print()
-
-# 0 == A, 1 == B, 2 == C, 3 == D
-G2.graph[0][1] = 1; G2.graph[0][2] = 1
-G2.graph[3][0] = 1; G2.graph[3][2] = 1
-
-print("G2 방향 그래프")
-for r in range(G2.size):
-    for c in range(G2.size):
-        print(G2.graph[r][c], end=' ')
-    print()
-
-# 0 == A, 1 == B, 2 == C, 3 == D
-G_self.graph[0][3] = 1
-G_self.graph[1][2] = 1; G_self.graph[1][3] = 1
-G_self.graph[2][1] = 1
-G_self.graph[3][0] = 1; G_self.graph[3][1] = 1
-
-print("G_self 무방향 그래프")
-for r in range(G_self.size):
-    for c in range(G_self.size):
-        print(G_self.graph[r][c], end=' ')
-    print()
+visited_dfs = [0 for _ in range(len(graph))]
+dfs(graph, 4, visited_dfs)
