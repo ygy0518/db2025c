64ccab34cb0d7d6b02b45080ce7ee134bcc0d314
unknown <ygy0518@gmail.com>
2025-05-22 15:50:03 +0900

    v4.2 graph

--- week11.py
+++ week11.py
@@ -5,6 +5,9 @@ class Graph:
 
 
 G1 = Graph(4)
+G2 = Graph(4)
+G_self = Graph(4)
+
 # 0 == A, 1 == B, 2 == C, 3 == D
 G1.graph[0][1] = 1; G1.graph[0][2] = 1; G1.graph[0][3] = 1
 G1.graph[1][0] = 1; G1.graph[1][2] = 1
@@ -15,4 +18,26 @@ print("G1 무방향 그래프")
 for r in range(G1.size):
     for c in range(G1.size):
         print(G1.graph[r][c], end=' ')
+    print()
+
+# 0 == A, 1 == B, 2 == C, 3 == D
+G2.graph[0][1] = 1; G2.graph[0][2] = 1
+G2.graph[3][0] = 1; G2.graph[3][2] = 1
+
+print("G2 방향 그래프")
+for r in range(G2.size):
+    for c in range(G2.size):
+        print(G2.graph[r][c], end=' ')
+    print()
+
+# 0 == A, 1 == B, 2 == C, 3 == D
+G_self.graph[0][3] = 1
+G_self.graph[1][2] = 1; G_self.graph[1][3] = 1
+G_self.graph[2][1] = 1
+G_self.graph[3][0] = 1; G_self.graph[3][1] = 1
+
+print("G_self 무방향 그래프")
+for r in range(G_self.size):
+    for c in range(G_self.size):
+        print(G_self.graph[r][c], end=' ')
     print()
